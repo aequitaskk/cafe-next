fc45dbfb71c9d678f108098914d817660d5268f1
dalmedian27 <kkla1124@gmail.com>
2023-05-24 23:07:52 -0400

    init

---
+++ components/Top.jsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Top = () => {
+  return (
+    <section id="home" className="h-screen w-screen relative scroll-smooth overflow-x-hidden">
+      <div className="absolute top-0 h-full w-full">
+        <Image
+          src="/images/bg.png"
+          alt="background"
+          fill
+          className="object-cover"
+        />
+      </div>
+      <div className="relative h-[70vh] z-[10] flex justify-center items-center">
+        <div className="flex flex-col justify-center items-center text-center  text-white">
+          <h1 className="text-5xl">SERVE YOU BETTER TASTE OF COFFEE</h1>
+          <p className="text-2xl mt-2 mb-10 font-md tracking-wider">
+            fresh place to drink coffee
+          </p>
+          <Link
+            href="#menu"
+            scroll={false}
+            className="bg-gray-800 px-[2rem] py-[8px] rounded-xl scroll-smooth"
+          >
+            <p className="font-semibold tracking-wider">MENU</p>
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Top;
